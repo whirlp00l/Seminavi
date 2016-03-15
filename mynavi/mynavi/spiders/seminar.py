e2c8a8a60073a890ce9fd185add2111f7ae8dcc5
Yi LU <luyi.eric@gmail.com>
2016-03-15 20:33:23 +0900

    add basic extraction

--- mynavi/mynavi/spiders/seminar.py
+++ mynavi/mynavi/spiders/seminar.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-
+import re
 
 class SeminarSpider(scrapy.Spider):
 	name = "seminar"
 	allowed_domains = ["job.mynavi.jp"]
 
 	def start_requests(self):
-		f = open('mynavi/spiders/companyID.txt','r')
+		f = open('mynavi/spiders/test.txt','r')
 		companyID = f.readline()
 		IDList = companyID.split(',')
 		for id in IDList:
@@ -16,4 +15,20 @@ class SeminarSpider(scrapy.Spider):
 			yield scrapy.Request(url, self.parse)
 
 	def parse(self, response):
-		pass
+		match = re.search('/displaySeminarList/',response.url)
+		if match:
+			urls = response.xpath('//div[@class="internList splitEntry"]//@href').extract()
+			for url in urls:
+				url = response.urljoin(url)
+				yield scrapy.Request(url, self.parse)
+		else:
+			self.parse_item(response)
+
+	def parse_item(self, response):
+		url = response.url
+		title = response.xpath('//headerWrap//h3/text()').extract()
+		date = response.xpath('//td[@class="date"]/text()').extract()
+		time = response.xpath('//td[@class="time"]/text()').extract()
+		place = response.xpath('//td[@class="place"]/text()').extract()
+		target = response.xpath('//td[@class="area"]/text()').extract()
+		submit = response.xpath('//td[@class="submit"]//@herf').extract()
